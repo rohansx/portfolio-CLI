cad880778d4563f03b86432ccd3402f1aeb1df13
rohansx <hello@rohan.sh>
2025-03-31 02:25:11 +0530

    fix build

--- config-overrides.js
+++ config-overrides.js
@@ -1,6 +1,21 @@
 const webpack = require("webpack");
 
 module.exports = function override(config, env) {
+  // Fix for webpack-manifest-plugin
+  if (env === "production") {
+    // Find the ManifestPlugin instance
+    const manifestPluginIndex = config.plugins.findIndex(
+      (plugin) =>
+        plugin.constructor &&
+        plugin.constructor.name === "WebpackManifestPlugin"
+    );
+
+    if (manifestPluginIndex !== -1) {
+      // Remove the problematic plugin
+      config.plugins.splice(manifestPluginIndex, 1);
+    }
+  }
+
   // Add support for MDX files
   config.module.rules.push({
     test: /\.mdx?$/,
